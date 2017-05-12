50c5ff43587556a866532d3a1420418f026782aa
debarros <pj.debarros@gmail.com>
2017-05-12 08:49:40 -0400

    Students.R - changed some variable names, added section to identify cohort issues

--- Students.R
+++ Students.R
@@ -1,43 +1,66 @@
 #Students.R
 
+#First run MainScript.R
+
+#########################################
+# Reconcile Workbook and PowerSchool ####
+#########################################
+
 #This script finds discrepancies between PowerSchool and the Accountability Workbook in terms of which students are current
 
 #Export a ton of info from the Students table in PS
 psStudentsRaw = read.csv("PowerSchoolStudents.csv")
 
-#Get a list of the students active in PS 
+#Get a list of the students in PS 
 InPS = psStudentsRaw$student_number
+
+#Get a list of the students active in PS 
+ActiveInPS = psStudentsRaw$student_number[psStudentsRaw$Enroll_Status == 0]
 
 #Get a list of the students from the workbook 
 InWorkbook = Workbook$`Local.ID.(optional)`
 
-#Get a list of the students who are in both the Workbook and PS
-inBoth = intersect(Workbook$`Local.ID.(optional)`, psStudentsRaw$student_number)
-
 #Get a list of the students from the workbook who are active 
 ActiveInWorkbook = Workbook$`Local.ID.(optional)`[(Workbook$`Still.Enrolled?` == "yes") ]
+
+#Get a list of the students who are in both the Workbook and PS
+inBoth = intersect(InWorkbook, InPS)
 
 #Get a list of the students from the workbook who are inactive 
 InactiveInWorkbook = setdiff(InWorkbook, ActiveInWorkbook)
 
 #Get a list of the students who are enrolled in PS, appear in the wkbk, but are not enrolled in the wkbk
-InPSNotActiveInWkbk = intersect(InPS, InactiveInWorkbook)
+ActiveInPS.InactiveInWkbk = intersect(ActiveInPS, InactiveInWorkbook)
 
 #Get a list of students who are enrolled in PS but nowhere in the wkbk
-InJustPS = setdiff(InPS, InWorkbook)
+ActiveInPS.NotInWorkbook = setdiff(ActiveInPS, InWorkbook)
 
 #Get a list of students who are active in the workbook, appear in PS, but are not enrolled in PS
-#write code here
+ActiveInWorkbook.InactiveInPS = setdiff(intersect(ActiveInWorkbook,InPS), ActiveInPS)
 
 #Get a list of students who are active in the workbook, but are not in PS at all
-#write code here
+ActiveInWorkbook.NotInPS = setdiff(ActiveInWorkbook, InPS)
 
 #Compile information into one readable table
 #write code here
 
+#############################
+# Identify Cohort Issues ####
+#############################
+
+Workbook.InCohort = Workbook[toupper(Workbook$`Included.in.Graduation.Cohort?`) == "YES",]
+unique(Workbook.InCohort$`Cohort.Year.(year.1st.entered.9th)`)
+Workbook.InCohort.Unenrolled = Workbook.InCohort[toupper(Workbook.InCohort$`Still.Enrolled?`) == "NO",]
+unique(Workbook.InCohort.Unenrolled$`Cohort.Year.(year.1st.entered.9th)`)
+Workbook.Problems = Workbook.InCohort.Unenrolled[!VbetterComp(toupper(Workbook.InCohort.Unenrolled$Discharge.Reason), "GRADUATED"),]
+unique(Workbook.Problems$`Cohort.Year.(year.1st.entered.9th)`)
+Workbook.Problems = Workbook.Problems[Workbook.Problems$`Cohort.Year.(year.1st.entered.9th)` >= 2012,]
+Workbook.Problems = Workbook.Problems[,c("Local.ID.(optional)","Last.Name","First.Name","Cohort.Year.(year.1st.entered.9th)","Date.left.GTH","Discharge.Reason")]
 
 
-
+##################################
+# Identify Off-Track Students ####
+##################################
 
 
 
