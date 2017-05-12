97429eac9cdf46b41ea137edd0e24ba5af8a49b0
debarros <pj.debarros@gmail.com>
2017-05-12 08:48:37 -0400

    MainScript.R - added some notes

--- MainScript.R
+++ MainScript.R
@@ -18,8 +18,44 @@ Workbook = Workbookraw.xlsx[!is.na(Workbookraw.xlsx$`Local.ID.(optional)`),-c(1,
 # Remove those who never attended
 # Note: don't do this if you are updating regents scores or other things in the workbook
 Workbook = Workbook[!(VbetterComp(Workbook$Discharge.Reason, "never attended")),]
+rownames(Workbook) = NULL
 
 #Format the date variables
 dateVars = c("Date.First.Enrolled.at.GTH", "Date.left.GTH", "Date.1st.Enrolled.in.9th.Grade.(anywhere)","Date.of.Confirmation")
 for(i in dateVars){ Workbook[,i] = as.Date(Workbook[,i], origin = "1899-12-30") }
+
+
+
+# Below are variables that are important to get from powerschool
+# Note that the code currently assumes subsets of this data for the powerschool regents and powerschool students files
+# 
+# student_number
+# lastfirst
+# grade_level
+# RCT_Reading_Score
+# RCT_Global_History_&_Geography_Score
+# Regents_Spanish_Score
+# Regents_Global_History_&_Geography_Score
+# Regents_Earth_Science_Score
+# Regents_French_Score
+# RCT_US_History_&_Government
+# RCT_Science_Score
+# RCT_Math_Score
+# RCT_Writing_Score
+# Regents_US_History_Score
+# Regents_Algebra_Score
+# Regents_Chemistry_Score
+# Regents_Geometry_Score
+# Regents_Living_Environment_Score
+# Regents_Algebra2/Trigonometry_Score
+# Regents_Comprehensive_English_Score
+# Regents_Physics_Score
+# Last_Name
+# First_name
+# DistrictEntryDate
+# Enroll_Status
+# EntryDate
+# Ethnicity
+# ExitCode
+# ExitDate
 
