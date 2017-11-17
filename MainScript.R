a0dad9db0cd2be676f6a897080c2600edc591713
debarros <pj.debarros@gmail.com>
2017-11-17 08:56:07 -0500

    Reorganized Mainscript.R and put data file locations in functions.R

--- MainScript.R
+++ MainScript.R
@@ -1,44 +1,59 @@
 # MainScript.R
+# This script must be run before any other script is run
 
-#---------------------------#
-#### Load and clean data ####
-#---------------------------#
 
-# This section must be run before any other script is run
 
+#-----------------------------------------#
+#### Load libraries and file locations ####
+#-----------------------------------------#
 source("functions.R")
 
+
+
+#-------------------------------#
+#### Accountability Workbook ####
+#-------------------------------#
 # Get the most up to date stuff from the actual workbook
 # Note: you must have access to the data drive on the school network
 Workbookraw.xlsx = read.xlsx(
-  xlsxFile = "\\\\stuthin2\\Data\\Accountability Spreadsheet\\working copy\\Green Tech Cohort Data Collection Workbook.xlsx",
+  xlsxFile = AcctWkBkLocation,
   sheet = "All info in 1 sheet",startRow = 2)
 Workbook = Workbookraw.xlsx[!is.na(Workbookraw.xlsx$`Local.ID.(optional)`),-c(1,2,3)]
-
-
 #Format the date variables
 dateVars = c("Date.First.Enrolled.at.GTH", "Date.left.GTH", "Date.1st.Enrolled.in.9th.Grade.(anywhere)","Date.of.Confirmation")
 for(i in dateVars){  Workbook[,i] = as.Date(Workbook[,i], origin = "1899-12-30") }
 
+
+
+#---------------------------------#
+#### PowerSchool Student Table ####
+#---------------------------------#
 # Read in student data from powerschool
 # Note: update the data in PowerSchoolAll.xlsx before loading it
 # There is a tab in the file that shows what fields to export
-powerschoolraw = read.xlsx(xlsxFile = "PowerSchoolAll.xlsx", sheet = 1)
+powerschoolraw = read.xlsx(xlsxFile = PSLocation, sheet = 1)
 
 
+
+#-----------------------------#
+#### PowerSchool F2 grades ####
+#-----------------------------#
 # Export all F2 grades from the storedgrades table in PowerSchool
 # Note: The export will take a really long time
-F2 = read.xlsx(xlsxFile = "PowerSchoolAll.xlsx", sheet = "F2 Grades")
+F2 = read.xlsx(xlsxFile = PSLocation, sheet = "F2 Grades")
 F2 = F2[F2$`[1]Student_Number` != 0,]
 F2$DateStored = as.Date(F2$DateStored, origin = "1899-12-30")
 F2$StudentName = powerschoolraw$lastfirst[match(F2$`[1]Student_Number`, table = powerschoolraw$student_number)]
 
 
+
+#---------------------------------#
+#### Course-Subject Alignments ####
+#---------------------------------#
 # Sign in to google
 gs_auth() #this may launch a browser so you can sign into your account
-
 # Get the course-subject alignments
-CourseSubject = gs_url("https://docs.google.com/a/greentechhigh.org/spreadsheets/d/17QhVYZkjbx34M6wBvtHUYa_XrRUlRbOtuOsQ4P5l-nk/edit?usp=sharing")
+CourseSubject = gs_url(CourseSubjectAddress)
 alignment = gs_read(ss = CourseSubject, ws = 1)
 alignment2 = gs_read(ss = CourseSubject, ws = 2)
 alignment$Course[alignment$Course == "AP Global History I"][1] = "AP Global History I "
@@ -46,23 +61,31 @@ FullAlignment = rbind.data.frame(alignment, alignment2, stringsAsFactors = F)
 
 
 
+#------------------------#
+#### Regents Database ####
+#------------------------#
 #Get the the Output query from the Regents database (located at data drive/database project/regents.accdb)
 #Paste it into the files RegentsDB.csv in the folder for this project
 #note: if new exams (not just new scores) have been entered, 
 #      the sql code for the Output query will need to be modified 
 #      to pull that exam's scores and dates from TestScoreOutput and TestDateOutput
-RegentsDBraw = read.csv("RegentsDB.csv", stringsAsFactors = FALSE) 
+RegentsDBraw = read.csv(RDBLocation, stringsAsFactors = FALSE) 
 
 
 
-
+#----------------------------#
+#### PowerSchool CC Table ####
+#----------------------------#
 # Load the current term's enrollment records (exported from the cc table in powerschool)
-cc.raw = read.xlsx(xlsxFile = "PowerSchoolAll.xlsx", sheet = "cc")
+cc.raw = read.xlsx(xlsxFile = PSLocation, sheet = "cc")
 
 
+
+#----------------------#
+#### SIRS Templates ####
+#----------------------#
 # Load all the templates (data dictionaries) for the SIRS exports
-wblocation = "\\\\stuthin2/Data/SIRS manuals templates guides etc/2017-18eScholarTemplatesNYS_2017-09-25.xlsx"
-templates = loadWorkbook(wblocation)
+templates = loadWorkbook(TemplateLocation)
 
 
 
--- functions.R
+++ functions.R
@@ -1,12 +1,25 @@
 #functions.R
 
-library("abind")
-library("ggplot2")
-library("scales")
-library("reshape2")
-library("openxlsx")
+library(abind)
+library(ggplot2)
+library(scales)
+library(reshape2)
+library(openxlsx)
 library(dBtools)
 library(googlesheets)
 library(data.table)
-source("CreateCompareMatrix.R")
 
+# Location of the CSI accountability workbook
+AcctWkBkLocation = "\\\\stuthin2\\Data\\Accountability Spreadsheet\\working copy\\Green Tech Cohort Data Collection Workbook.xlsx"
+
+# Location of the eScholar templates for the SIRS exports
+TemplateLocation = "\\\\stuthin2/Data/SIRS manuals templates guides etc/2017-18eScholarTemplatesNYS_2017-09-25.xlsx"
+
+# URL of the Course-Subject alignment table
+CourseSubjectAddress = "https://docs.google.com/a/greentechhigh.org/spreadsheets/d/17QhVYZkjbx34M6wBvtHUYa_XrRUlRbOtuOsQ4P5l-nk/edit?usp=sharing"
+
+# Location of the PowerSchool exports file
+PSLocation = "PowerSchoolAll.xlsx"
+
+# Location of the Regents Database export file
+RDBLocation = "RegentsDB.csv"
