85982572368355c42f841b16e8e52e0aa769d241
debarros <pj.debarros@gmail.com>
2017-11-30 10:47:28 -0500

    Level0 script - added code to output a file of students who are missing race/ethnicity info

--- Level0.R
+++ Level0.R
@@ -14,12 +14,23 @@ colnames(demographics) = x[1:ncol(demographics)]
 
 
 # reformat the demographics file
-demographics[which(is.na(demographics), arr.ind = T)] = ""                                        # make blank spaces blank (not NA)
-demographics[,8] = as.character(demographics[,8])                                                 # make Grade Level character to preserve 0's
-demographics[nchar(demographics[,8]) == 1,8] = "09"                                               # change single-digit grade levels to "09"
-demographics[demographics[,24] == "68",24] = "068"                                                # change diploma type code 68 to 068
-demographics$ETHNICCODESHORTRACE1CODE[demographics$ETHNICCODESHORTRACE1CODE == ""] = "B"          # change missing race to Black
-write.table(demographics, "demographics.csv", row.names = F, col.names = F, sep = ",", dec = ".") # output 
+demographics[which(is.na(demographics), arr.ind = T)] = ""                                # make blank spaces blank (not NA)
+demographics[,8] = as.character(demographics[,8])                                         # make Grade Level character to preserve 0's
+demographics[nchar(demographics[,8]) == 1,8] = "09"                                       # change single-digit grade levels to "09"
+demographics[demographics[,24] == "68",24] = "068"                                        # change diploma type code 68 to 068
+
+if(any(demographics$ETHNICCODESHORTRACE1CODE == "")){                                     # Check for students missing race/ethnicity
+  write.csv(demographics[demographics$ETHNICCODESHORTRACE1CODE == "",4:8], 
+            "missing student race and ethnicity.csv")
+  print("There are students missing race and ethnicity.  Check the csv file.")
+}
+
+demographics$ETHNICCODESHORTRACE1CODE[demographics$ETHNICCODESHORTRACE1CODE == ""] = "B"  # change missing race to Black
+write.table(demographics, "demographics.csv", row.names = F,                              # output 
+            col.names = F, sep = ",", dec = ".") 
+
+
+
 
 
 #------------------#
