569e0891a48be31d2f137ecacd51fae600775f75
debarros <pj.debarros@gmail.com>
2017-12-18 11:09:48 -0500

    DemographicChanges.R - lots of modifications.  Mostly did stuff to use actual column names of the extract pulled from the template.

--- DemographicChanges.R
+++ DemographicChanges.R
@@ -1,36 +1,32 @@
 # DemographicChanges.R
 
-files = list.files("J:/2016-2017/Level 0/student lite (demographics)", full.names = T)  
-demog = vector(mode = "list", length = length(files))
-nchar(files[1])
-
-
-for(i in 1:length(files)){
-  demog[[i]] = read.csv(file = files[i], header = F, stringsAsFactors = F)
-  names(demog)[i] = substr(files[i],73,86)
-}
-
-
-demog.combo = rbindlist(l = demog, idcol = "version")
+demofolder = "\\\\stuthin2/data/2017-2018/Level 0/student lite (demographics)"
+demog.combo = read.csv.multi(folder = demofolder, header = F, idcol = "version")
 demog.combo = as.data.frame(demog.combo)
-demog.combo$Address = paste0(demog.combo$V29, ", ",demog.combo$V31, ", ",demog.combo$V33)
-students = data.frame(StudentID = unique(demog.combo$V4))
-students$LastName = demog.combo$V5[match(students$StudentID, demog.combo$V4)]
-students$FirstName = demog.combo$V6[match(students$StudentID, demog.combo$V4)]
-students$StreetChange = F #V29
-students$MuniChange = F #V31
-students$ZipChange = F #V33
+colnames(demog.combo) = c("version",GetNiceColumnNames("STUDENT LITE", templates))
+
+
+addrFields = c("ADDRESS1STUDENTSADDRESSLINE1", "CITYSTUDENTSADDRESSCITY", "ZIPCODESTUDENTSADDRESSZIPCODE", "DISTRICTCODEOFRESIDENCE")
+demog.combo$Address = apply(X = demog.combo[,addrFields], MARGIN = 1, FUN = paste0, collapse = ", ")
+students = data.frame(StudentID = unique(demog.combo$STUDENTIDSCHOOLDISTRICTSTUDENTID))
+students$LastName = demog.combo$LASTNAMESHORTSTUDENTSLASTNAME[match(students$StudentID, demog.combo$STUDENTIDSCHOOLDISTRICTSTUDENTID)]
+students$FirstName = demog.combo$FIRSTNAMESHORTSTUDENTSFIRSTNAME[match(students$StudentID, demog.combo$STUDENTIDSCHOOLDISTRICTSTUDENTID)]
+students$StreetChange = F 
+students$MuniChange = F 
+students$ZipChange = F 
+students$DORChange = F
 studentIssues = demog.combo[0,]
 emptyRow = demog.combo[1,]
 for(i in 1:ncol(emptyRow)){
   emptyRow[1,i] = ""
 }
 for(i in 1:nrow(students)){
-  currentSet = demog.combo[demog.combo$V4 == students$StudentID[i],]
-  if(length(unique(currentSet$V29))>1) students$StreetChange[i] = T
-  if(length(unique(currentSet$V31))>1) students$MuniChange[i] = T
-  if(length(unique(currentSet$V33))>1) students$ZipChange[i] = T
-  if(any(unlist(students[i,c("StreetChange", "MuniChange", "ZipChange")]))){
+  currentSet = demog.combo[demog.combo$STUDENTIDSCHOOLDISTRICTSTUDENTID == students$StudentID[i],]
+  if(length(unique(currentSet$ADDRESS1STUDENTSADDRESSLINE1))>1)  students$StreetChange[i] = T
+  if(length(unique(currentSet$CITYSTUDENTSADDRESSCITY))>1)       students$MuniChange[i] = T
+  if(length(unique(currentSet$ZIPCODESTUDENTSADDRESSZIPCODE))>1) students$ZipChange[i] = T
+  if(length(unique(currentSet$DISTRICTCODEOFRESIDENCE))>1)       students$DORChange[i] = T
+  if(any(unlist(students[i,c("StreetChange", "MuniChange", "ZipChange", "DORChange")]))){
     currentSet = currentSet[!duplicated(currentSet$Address),]
     studentIssues = rbind.data.frame(studentIssues, currentSet, stringsAsFactors = F)
     studentIssues = rbind.data.frame(studentIssues, emptyRow, stringsAsFactors = F)
@@ -41,7 +37,7 @@ students$AnyChange = apply(students[,c("StreetChange", "MuniChange", "ZipChange"
 
 write.csv(x = students[students$AnyChange,1:3], file = "StudentsWithAddressChanges.csv")
 
-studentIssues = studentIssues[,c("version", "V5","V6","Address")]
+studentIssues = studentIssues[,c("version", "STUDENTIDSCHOOLDISTRICTSTUDENTID","FIRSTNAMESHORTSTUDENTSFIRSTNAME","LASTNAMESHORTSTUDENTSLASTNAME","Address")]
 write.csv(x = studentIssues, file = "AllAddressChanges.csv", row.names = F)
 
 
