2c8c45fcf5fa02601a4cc0c319c0838bc75c86ed
debarros <pj.debarros@gmail.com>
2018-02-13 11:46:02 -0500

    ASAP to Access.R - added lots of notes and stuff at the beginning.  Created the section that converts regents.xlsx files into a csv that can be pasted into the regents database.

--- ASAP to Access.R
+++ ASAP to Access.R
@@ -1,28 +1,32 @@
 # ASAP to Access.R
 
+# This script is for pulling regents data from ASAP and putting it into the regents database
+# Ideally, it would involve one step: ASAP -> Access
+# For the time being, it works more like this:
+#    1. ASAP -> downloaded csv
+#    2. downloaded csv -> regents.xlsx
+#    3. regents.xslx -> combined csv
+#    4. combined csv -> regents database
+# This script currently only covers step 3.  Steps 1, 2, and 4 must be done manually.
+
+# Until step 4 is implemented, the output should be a CSV with the following columns:
+# ID, Exam, Score, Grade, Session, Location, InfoSource
+#    Exam should match the values in the Exams table of the database
+#    Score should be on a scale of 0 to 100 with no decimal point
+#    Grade should be the student's grade level (studentInfo$grade_level)
+#    Session should be YYYY Month (e.g. "2018 January")
+#    Location should be "GTH"
+#    InfoSource should be "ASAP"
 
 
 
-#--------------------------#
-#### Straight from ASAP ####
-#--------------------------#
 
-# Extract data from ASAP exports and generate stuff to paste into the Regents database.
+#---------------------------------#
+#### General Purpose Variables ####
+#---------------------------------#
 
 currentSession = "2018-01"
-
-
-files = list.files(path = paste0("//stuthin2/Data/regents score exports/",currentSession), full.names = T, recursive = T)
-files = grep(pattern = "ASAPStudentItemAnalysisMulti", x = files, ignore.case = T, value = T)
-
-allScores = vector(mode = "list", length = length(files))
-
-filename = files[1]
-
-for(filename in files){
-  results = read.csv(file = filename, skip = 3, stringsAsFactors = F)
-}
-
+studentInfo = powerschoolraw
 
 
 
@@ -34,9 +38,41 @@ for(filename in files){
 # Extract data from regents.xslx workbooks and generate stuff to paste into the Regents database.
 files = list.files(path = paste0("//stuthin2/Data/regents score exports/",currentSession), full.names = T, recursive = T)
 files = grep(pattern = "regents.xlsx", x = files, ignore.case = T, value = T)
-files = grep(pattern = "~$", x = files, ignore.case = T, value = T)
+files = files[grep(pattern = "~$", x = files, fixed = T, invert = T)]
 
-filename = files[2]
+
+results = vector(mode = "list", length = length(files))
+for(i in 1:length(files)){
+  filename = files[i]
+  results[[i]] = read.xlsx(xlsxFile = filename, sheet = "Scores for pasting")
+}
+
+results = rbindlist(results)
+results$Score = as.integer(100 * results$Scaled.score.as.percentage)
+results$TestName2 = substr(results$Test.Name, 1, nchar(results$Test.Name) - 6)
+results$session = paste0(substr(currentSession, 1, 4), " ", month.name[as.integer(substr(currentSession, 6, 7))])
+results$location = "GTH"
+results$InfoSource = "ASAP"
+results$Grade = studentInfo$grade_level[match(results$Student.ID, studentInfo$student_number)]
+
+
+results = results[,c("Student.ID", "TestName2", "Score", "Grade", "session", "location", "InfoSource")]
+
+unique(results$TestName2)
+
+#-------------------------#
+#### From ASAP Exports ####
+#-------------------------#
+
+# Extract data from ASAP exports and generate stuff to paste into the Regents database.
+
+files = list.files(path = paste0("//stuthin2/Data/regents score exports/",currentSession), full.names = T, recursive = T)
+files = grep(pattern = "ASAPStudentItemAnalysisMulti", x = files, ignore.case = T, value = T)
+
+allScores = vector(mode = "list", length = length(files))
+
+filename = files[1]
+
 for(filename in files){
-  results = read.xlsx(xlsxFile = filename, sheet = "Scores for pasting")
+  results = read.csv(file = filename, skip = 3, stringsAsFactors = F)
 }
