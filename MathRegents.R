ccfeca63f7b4e58c910b1dc7ea737f3456ec4fe7
debarros <pj.debarros@gmail.com>
2018-11-20 14:11:28 -0500

    MathRegents.R - major changes.  Added error handling; added precalc to the table of courses; added ability to handle January testing session; shortened variable names.

--- MathRegents.R
+++ MathRegents.R
@@ -1,13 +1,19 @@
 # MathRegents.R
 
 # This script determines which students should be signed up for each math regents exam
-# It requires the current year cc table, all F2 grades, the table of current students, and the regents database
+# It requires the current year cc table, all F2 grades, the table of current students, 
+# the regents database, and course-subject alignments
 
-Students = currentStudents # make a copy of the table of current students
-cc.cur = cc.raw[cc.raw$TermID > 0,] # make a copy of the cc table, limiting to current enrollments
-cc.cur$dept = FullAlignment$Subject[match(cc.cur$`[02]course_name`, FullAlignment$Course)] # add course-subject alignments
-cc.cur.math = cc.cur[cc.cur$dept == "Math",] # limit to just math
-cc.cur.math = cc.cur.math[cc.cur.math$`[02]course_name` != "Exploring Algebra with Technology",] # remove EAT
+Session = "January" # must be "January" or "June"
+
+
+Students = currentStudents                                                        # copy table of current students
+cc.cur = cc.raw[cc.raw$TermID > 0,]                                               # limit cc table to current enrollments
+colnames(cc.cur)[colnames(cc.cur) == "[02]course_name"] = "CN"                    # shorten column names
+colnames(cc.cur)[colnames(cc.cur) == "[01]Student_Number"] = "StudNum"
+cc.cur$dept = FullAlignment$Subject[match(cc.cur$CN, FullAlignment$Course)]       # add course-subject alignments
+cc.cur.math = cc.cur[cc.cur$dept == "Math",]                                      # limit to just math
+cc.cur.math = cc.cur.math[cc.cur.math$CN != "Exploring Algebra with Technology",] # remove EAT
 row.names(cc.cur.math) = NULL
 
 
@@ -23,31 +29,40 @@ Regents$Alg2 = apply(Regents[,c("TrigScore", "Alg2CCScore")], 1, betterMax)
 
 # Limit the stored F2 grades to just math grades
 F2$dept = FullAlignment$Subject[match(F2$Course_Name, FullAlignment$Course)] # add course-subject alignments
-F2 = F2[!is.na(F2$dept),]
-F2 = F2[F2$dept == "Math",]
-F2 = F2[F2$Course_Name != "Exploring Algebra with Technology",]
-F2 = F2[!is.na(F2$Grade),]
+F2 = F2[!is.na(F2$dept),]                                                    # remove those with no dept
+F2 = F2[F2$dept == "Math",]                                                  # limit to math
+F2 = F2[F2$Course_Name != "Exploring Algebra with Technology",]              # remove EAT
+F2 = F2[!is.na(F2$Grade),]                                                   # remove those with no letter grade
+colnames(F2)[colnames(F2) == "[1]Student_Number"] = "StudNum"                # shorten column name
+
 
 
 # This is not a great way to make this data.frame
+# "Rel" appears to mean that everyone in that course should take that exam
+# "Jan" and "June" appear to mean that the student is eligible to take the exam
 MathCourses = data.frame(
   Course = c("Algebra A", "Integrated Algebra I- 1 credit", "Intermediate Algebra 10", "Geometry A", "Geometry 1 credit", 
-             "Algebra II/Trig- 1 credit", "Intermediate Trig", "Topics in Math/ Finite Math"),
-  Order = 1:8,
-  JuneAlg1 = c(F, rep(T, 7)),
-  JanAlg1 = c(F,F, rep(T, 6)),
-  RelAlg1 = c(F, rep(T, 2), rep(F, 5)),
-  JuneGeom = c(rep(F, 4), rep(T, 4)),
-  JanGeom = c(rep(F, 4), rep(T, 4)),
-  RelGeom = c(rep(F, 4), T, rep(F, 3)),
-  JuneAlg2 = c(rep(F, 5), rep(T, 3)),
-  JanAlg2 = c(rep(F, 6), rep(T, 2)),
-  RelAlg2 = c(rep(F, 5), rep(T, 2), F),
+             "Algebra II/Trig- 1 credit", "Intermediate Trig", "Topics in Math/ Finite Math", "Pre-Calculus 1-credit"),
+  Order = 1:9,
+  JuneAlg1 = c(F, rep(T, 8)),
+  JanAlg1 = c(F,F, rep(T, 7)),
+  RelAlg1 = c(F, rep(T, 2), rep(F, 6)),
+  JuneGeom = c(rep(F, 4), rep(T, 5)),
+  JanGeom = c(rep(F, 5), rep(T, 4)),
+  RelGeom = c(rep(F, 4), T, rep(F, 4)),
+  JuneAlg2 = c(rep(F, 5), rep(T, 4)),
+  JanAlg2 = c(rep(F, 6), rep(T, 3)),
+  RelAlg2 = c(rep(F, 5), rep(T, 2), rep(F,2)),
   stringsAsFactors = F
 )
 
 # This should be character(0).  If not, additional rows must be added to the MathCourses data.frame
-setdiff(unique(cc.cur.math$`[02]course_name`), MathCourses$Course)
+if(identical(setdiff(unique(cc.cur.math$CN), MathCourses$Course), character(0))){
+  print("Everything is fine")
+} else {
+  print("It needs to be added to the MathCourses data.frame")
+}
+
 
 
 # Add the necessary variables to the Students table
@@ -72,12 +87,14 @@ Students$HighestExamTaken[!is.na(Students$BestAlg1)] = "Alg1"
 Students$HighestExamTaken[!is.na(Students$BestGeom)] = "Geom"
 Students$HighestExamTaken[!is.na(Students$BestAlg2)] = "Alg2"
 
+
+
 for(i in 1:nrow(Students)){
-  thisCourses = cc.cur.math$`[02]course_name`[cc.cur.math$`[01]Student_Number` == Students$Student_Number[i]]
+  thisCourses = cc.cur.math$CN[cc.cur.math$StudNum == Students$Student_Number[i]]
   thisCourses = MathCourses[MathCourses$Course %in% thisCourses,]
   
   passedCourses = F2[F2$Grade != "F",]
-  passedCourses = passedCourses$Course_Name[passedCourses$`[1]Student_Number` == Students$Student_Number[i]]
+  passedCourses = passedCourses$Course_Name[passedCourses$StudNum == Students$Student_Number[i]]
   passedCourses = MathCourses[MathCourses$Course %in% passedCourses,]
   
   Students$MathCourseCount[i] = nrow(thisCourses)
@@ -89,16 +106,29 @@ for(i in 1:nrow(Students)){
     Students$HighPassedMathCourse[i] = passedCourses$Course[passedCourses$Order == max(passedCourses$Order)]  
   }
   
-  Students$EligibleAlg1[i] = any(c(passedCourses$JuneAlg1, thisCourses$JuneAlg1))
-  Students$EligibleGeom[i] = any(c(passedCourses$JuneGeom, thisCourses$JuneGeom))
-  Students$EligibleAlg2[i] = any(c(passedCourses$JuneAlg2, thisCourses$JuneAlg2))
+  # Use the Session to determine whether the student is eligible for each exam
+  if(Session == "June"){
+    Students$EligibleAlg1[i] = any(c(passedCourses$JuneAlg1, thisCourses$JuneAlg1))
+    Students$EligibleGeom[i] = any(c(passedCourses$JuneGeom, thisCourses$JuneGeom))
+    Students$EligibleAlg2[i] = any(c(passedCourses$JuneAlg2, thisCourses$JuneAlg2))  
+  } else if (Session == "January") {
+    Students$EligibleAlg1[i] = any(c(passedCourses$JuneAlg1, thisCourses$JanAlg1))
+    Students$EligibleGeom[i] = any(c(passedCourses$JuneGeom, thisCourses$JanGeom))
+    Students$EligibleAlg2[i] = any(c(passedCourses$JuneAlg2, thisCourses$JanAlg2))
+  } else {
+    stop("Session must be either June or January")
+  } # /if-else
   
   
-  if(any(c(Students$EligibleAlg1[i], !is.na(Students$BestAlg1[i])))){
-    if(any(c(is.na(Students$BestAlg1[i]), thisCourses$RelAlg1))){
-      Students$ShouldTakeAlg1[i] = T
-    } else if(Students$BestAlg1[i] < 65){
-      Students$ShouldTakeAlg1[i] = T
+  Students[i,]
+  
+  if(any(c(Students$EligibleAlg1[i], !is.na(Students$BestAlg1[i])))){   # Consider any student who has a score OR is eligible
+    
+    if(any(c(is.na(Students$BestAlg1[i]), thisCourses$RelAlg1))){       # If the student has no score OR is required to take it
+      Students$ShouldTakeAlg1[i] = T                                    # Sign him up
+      
+    } else if(Students$BestAlg1[i] < 65){                               # If the student's best score is below passing
+      Students$ShouldTakeAlg1[i] = T                                    # Sign him up
     }
   }
   
@@ -134,18 +164,18 @@ write.csv(Students, paste0(OutFolder, "math regents.csv"))
 #---------------------------------------------------------------------------------#
 
 # Just math
-F2$code = paste0(F2$`[1]Student_Number`, " - ", F2$Course_Name)
+F2$code = paste0(F2$StudNum, " - ", F2$Course_Name)
 F2.passed = F2[F2$Grade != "F",]
-cc.cur.math$code = paste0(cc.cur.math$`[01]Student_Number`, " - ", cc.cur.math$`[02]course_name`)
+cc.cur.math$code = paste0(cc.cur.math$StudNum, " - ", cc.cur.math$CN)
 intersect(F2.passed$code, cc.cur.math$code)
 View(F2.passed[F2.passed$code %in% cc.cur.math$code,])
 
 # All courses
-F2$code = paste0(F2$`[1]Student_Number`, " - ", F2$Course_Name)
+F2$code = paste0(F2$StudNum, " - ", F2$Course_Name)
 F2.passed = F2[!is.na(F2$Grade),]
 F2.passed = F2.passed[F2.passed$Grade != "F",]
 F2.passed = F2.passed[F2.passed$TermID < 2700,]
-cc.cur$code = paste0(cc.cur$`[01]Student_Number`, " - ", cc.cur$`[02]course_name`)
+cc.cur$code = paste0(cc.cur$StudNum, " - ", cc.cur$CN)
 intersect(F2.passed$code, cc.cur$code)
 View(F2.passed[F2.passed$code %in% cc.cur$code,])
 
