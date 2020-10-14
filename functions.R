24da2aeba1de74d92b3bdee66996b5eb8916c427
debarros <pj.debarros@gmail.com>
2020-10-14 07:52:36 -0400

    functions.R - updated some file locations, added library(tidyr), changed library(googlesheets) to library(googlesheets4)

--- functions.R
+++ functions.R
@@ -1,12 +1,13 @@
 #functions.R
 
+library(tidyr)
 library(dBtools)
 library(abind)
 SWSM(library(ggplot2, quietly = T, verbose = F))
 library(scales)
 library(reshape2)
 library(openxlsx)
-library(googlesheets)
+library(googlesheets4)
 library(stringr)
 library(googledrive)
 SWSM(library(data.table, quietly = T, verbose = F))
@@ -14,11 +15,13 @@ SWSM(library(data.table, quietly = T, verbose = F))
 # Location of the college board workbook
 CollegeBoardLocation = "\\\\stuthin2/Data/SAT's/college_board_data.xlsx"
 
+
+
 # Location of the CSI accountability workbook
 AcctWkBkLocation = "\\\\stuthin2\\Data\\Accountability Spreadsheet\\working copy\\GTH Data Collection Workbook.xlsx"
 
 # Location of the eScholar templates for the SIRS exports
-TemplateLocation = "\\\\stuthin2/Data/SIRS manuals templates guides etc/2019-20eSCHOLARTEMPLATES-07-26-2019.xlsx"
+TemplateLocation = "\\\\stuthin2/Data/SIRS manuals templates guides etc/2020-21-eSCHOLARTEMPLATES-08-21-2020.xlsx"
 
 # URL of the Course-Subject alignment table
 CourseSubjectAddress = "https://docs.google.com/a/greentechhigh.org/spreadsheets/d/17QhVYZkjbx34M6wBvtHUYa_XrRUlRbOtuOsQ4P5l-nk/edit?usp=sharing"
@@ -29,11 +32,11 @@ CurrentStudentsAddress = "https://drive.google.com/file/d/1EnnUpm2B-CZ1zYDyYoml6
 # Location of the PowerSchool exports file
 PSLocation = "PowerSchoolAll.xlsx"
 
-# Location of the Regents Database export file
-RDBLocation = "RegentsDB.csv"
+# Location of the Regents excel workbook
+RegentsLocation = "\\\\stuthin2/data/database project/AllRegents.xlsx"
 
 # Location of direct cert files from NYSSIS
-LunchLocation = "\\\\stuthin2/data/2019-2020\\lunch"
+LunchLocation = "\\\\stuthin2/data/2020-2021\\lunch"
 
 # Location of output files
 OutFolder = paste0(getwd(), "/Temporary Output/")
